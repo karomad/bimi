983d6f8954d01725ad03af59a8557c948772e4c7
Karapet Madoyan <karomadoyan@gmail.com>
2018-10-05 23:29:10 +0400

    hjhj

--- BIMI.Web/Models/JobModel.cs
+++ BIMI.Web/Models/JobModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,13 @@ namespace BIMI.Web.Models
     public class JobModel
     {
         public int ID { get; set; }
-        public int UserId { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
         public string Description { get; set; }
         public string MoneyRange { get; set; }
         public int Type { get; set; }
         public bool isParent { get; set; }
+       
+        public ApplicationUser User { get; set; }
     }
 }
