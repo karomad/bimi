d690099302acade225d2f3d2b5e4a777335002f3
Karapet Madoyan <karapet@7smarts.com>
2018-10-05 15:31:45 +0400

    dffff

---
+++ BIMI.Web/Enums/ServiceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BIMI.Web.Enums
+{
+    public enum ServiceType
+    {
+        Driver = 1,
+        BabySitter = 2,
+        Tutor = 3
+    }
+}
